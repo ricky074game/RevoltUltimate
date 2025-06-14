b4ff5d0d222f2e42e392031437c09084db4457fb
ricky074game <67124402+ricky074game@users.noreply.github.com>
2025-06-14 15:12:20 -0400

    Delete RevoltUltimate.Shared directory

--- RevoltUltimate.Shared/Contracts/IAchievementNotifier.cs
+++
@@ -1,9 +0,0 @@
-﻿using RevoltUltimate.API.Objects;
-
-namespace RevoltUltimate.API.Contracts
-{
-    public interface IAchievementNotifier
-    {
-        void ShowAchievement(Achievement achievement);
-    }
-}
